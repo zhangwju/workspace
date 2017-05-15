23942151ae09ea6530f99b9593bf0d573e872b96
root <root@localhost.localdomain>
2017-05-15 03:52:31 -0400

    add a misc

---
+++ misc/Makefile/test.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  i
+ *
+ *        Version:  1.0
+ *        Created:  07/21/2016 11:17:01 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("hello world\n");
+
+	return 0;
+}
